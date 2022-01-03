be8670b9fb3d126400bc9ad4e5eb567779da61a7
Boluwatife Omosowon <boluomosowon@gmail.com>
2022-01-03 19:07:32 +0100

    heroku fix

--- server.js
+++ server.js
@@ -23,19 +23,19 @@ app.get('/(:id)', function(req, res) {
 
 
 // set up redis server
-var redisClient;
-console.log(process.env.REDISTOGO_URL);
 if (process.env.REDISTOGO_URL) {
-  var rtg   = new URL(process.env.REDISTOGO_URL);
-  redisClient = require("redis").createClient(rtg.port, rtg.hostname);
-  redisClient.auth(rtg.auth.split(":")[1]);
+  var rtg = require("url").parse(process.env.REDISTOGO_URL);
+  var redis = require("redis").createClient(rtg.port, rtg.hostname);
+
+  redis.auth(rtg.client.auth.split(":")[1]);
 } else {
-  redisClient = require("redis").createClient();
+  var redis = require("redis").createClient();
 }
+
 
 // options for sharejs
 var options = {
-  db: {type: 'redis', client: redisClient}
+  db: {type: 'redis', client: redis}
 };
 
 
